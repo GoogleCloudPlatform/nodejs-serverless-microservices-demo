95685be3f1642366356a2d96fa528d5fcd757606
Steren Giannini <steren.giannini@gmail.com>
2018-03-26 18:31:34 -0700

    Creating Cron job using GAE cron

--- task-scheduler/server.js
+++ task-scheduler/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 var server = http.createServer((request, response) => {
+  console.log('Looking for new tasks to schedule...');
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello from Node.js!");
 });
