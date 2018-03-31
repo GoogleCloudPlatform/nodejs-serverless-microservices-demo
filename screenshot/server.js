b3128efd48a641d0a2cf131ca8358a64900b0cbc
Steren Giannini <steren.giannini@gmail.com>
2018-03-30 18:00:57 -0700

    Store screenshots in Cloud Storage

--- screenshot/server.js
+++ screenshot/server.js
@@ -1,13 +1,16 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
+const Storage = require('@google-cloud/storage');
+const storage = new Storage();
+
 const app = express();
 
 app.use(async (req, res) => {
-  if(req.path === '/') {res.end('Please provide URL');}
+  if(req.path === '/') {res.end('Please provide URL, example: /http://example.com');}
 
   const url = req.path.slice(1);
   console.log(`URL: ${url}`);
-
+  
   const browser = await puppeteer.launch({
     headless: true,
     timeout: 90000,
@@ -18,8 +21,20 @@ app.use(async (req, res) => {
   let imageBuffer = await page.screenshot();
   await browser.close();
 
-  // TODO: store in GCS
+  
+  // Uploads a local file to the bucket
+  
+  const bucketName = 'screenshots-microservice-demo';
+  const bucket = storage.bucket(bucketName);
+  
+  const date = new Date();
+  const timestamp = date.getTime();
+  const filename = `${timestamp}.png`;
+  const filepath = url.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+  const file = bucket.file(`${filepath}/${filename}`);
+  await file.save(imageBuffer);
 
+  // returns the screenshot
   res.set('Content-Type', 'image/png')
   res.end(imageBuffer);
 })
