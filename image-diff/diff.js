71479f50fa0351cfad2b6f39a9e126edf5eecc4b
Steren Giannini <steren.giannini@gmail.com>
2018-05-08 22:21:31 -0700

    Increase image-diff threshold

--- image-diff/diff.js
+++ image-diff/diff.js
@@ -18,7 +18,7 @@ const sharp = require('sharp');
 
 function diff(oldImage, newImage, length, width, callback) {
   // Number of pixels that have to mismatch to consider the images different
-  const diffPixelThreshold = 100;
+  const diffPixelThreshold = 100 * 100; // at least a change equivalent to 100x100px
 
   const diffBuffer = Buffer.alloc(oldImage.length);
   Promise.all([sharp(oldImage).toBuffer(), sharp(newImage).toBuffer()]).then(images => {
